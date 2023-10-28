769d1c6be037d37269ea7e597d94e62b2fdb0844
Hisashi Asakura <hasakura@bardavon.com>
2023-10-28 12:32:29 +0800

    Remove eks_ prefix in outputs.tf

--- modules/eks-control-plane/outputs.tf
+++ modules/eks-control-plane/outputs.tf
@@ -1,55 +1,65 @@
 ################################################################################
 # EKS Cluster
 ################################################################################
-output "eks_cluster_name" {
+output "cluster_name" {
   description = "The name of the EKS cluster."
   value       = aws_eks_cluster.this.name
 }
 
-output "eks_cluster_id" {
+output "cluster_id" {
   description = "The id of the EKS cluster."
   value       = aws_eks_cluster.this.id
 }
 
-output "eks_cluster_arn" {
+output "cluster_arn" {
   description = "The Amazon Resource Name (ARN) of the cluster."
   value       = aws_eks_cluster.this.arn
 }
 
-output "eks_cluster_endpoint" {
+output "cluster_endpoint" {
   description = "The endpoint for your EKS Kubernetes API."
   value       = aws_eks_cluster.this.endpoint
 }
 
-output "eks_cluster_version" {
+output "cluster_version" {
   description = "The Kubernetes server version for the EKS cluster."
   value       = aws_eks_cluster.this.version
 }
 
-output "eks_cluster_platform_version" {
+output "cluster_platform_version" {
   description = "Platform version for the cluster"
   value       = aws_eks_cluster.this.platform_version
 }
 
-output "eks_cluster_status" {
+output "cluster_status" {
   description = "Status of the EKS cluster. One of `CREATING`, `ACTIVE`, `DELETING`, `FAILED`"
   value       = aws_eks_cluster.this.status
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = try(aws_eks_cluster.this.certificate_authority[0].data, null)
+}
+
+output "cluster_primary_security_group_id" {
+  description = "Cluster security group that was created by Amazon EKS for the cluster. Managed node groups use this security group for control-plane-to-data-plane communication. Referred to as 'Cluster security group' in the EKS console"
+  value       = try(aws_eks_cluster.this.vpc_config[0].cluster_security_group_id, null)
 }
 
 ########################################
 ## IAM role for EKS Cluster Control Plane
 ########################################
-output "eks_cluster_iam_role_name" {
+output "cluster_iam_role_name" {
   description = "IAM role name of the EKS cluster"
   value       = aws_iam_role.cluster.name
 }
 
-output "eks_cluster_iam_role_arn" {
+output "cluster_iam_role_arn" {
   description = "IAM role ARN of the EKS cluster"
   value       = aws_iam_role.cluster.arn
 }
 
-output "eks_cluster_iam_role_unique_id" {
+output "cluster_iam_role_unique_id" {
   description = "Stable and unique string identifying the IAM role"
   value       = aws_iam_role.cluster.id
 }
@@ -57,17 +67,17 @@ output "eks_cluster_iam_role_unique_id" {
 ########################################
 ## KMS for K8s secret's DEK (data encryption key) encryption
 ########################################
-output "eks_secret_kms_arn" {
+output "cluster_secret_kms_arn" {
   description = "The Amazon Resource Name (ARN) of the key."
   value       = aws_kms_key.k8s_secret.arn
 }
 
-output "eks_secret_alias_arn" {
+output "cluster_secret_alias_arn" {
   description = "The Amazon Resource Name (ARN) of the key alias."
   value       = aws_kms_alias.k8s_secret.arn
 }
 
-output "eks_secret_id" {
+output "cluster_secret_id" {
   description = "The globally unique identifier for the key."
   value       = aws_kms_key.k8s_secret.id
 }
@@ -75,6 +85,6 @@ output "eks_secret_id" {
 ########################################
 ## CloudWatch Logs for EKS control plane logging
 ########################################
-output "eks_cluster_cloudwatch_logs_arn" {
+output "cluster_cloudwatch_logs_arn" {
   value = aws_cloudwatch_log_group.cluster.arn
 }
